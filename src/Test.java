e6f62ea3f3ae56066800cb709dfd8e4b284d8c7f
RanY <978697341@qq.com>
2020-01-05 00:09:39 +0800

    Update TrainingPreHandle

--- src/Test.java
+++ src/Test.java
@@ -3,9 +3,16 @@ import org.apache.hadoop.shaded.com.jcraft.jsch.IO;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.lang.System;
+import java.io.File;
 public class Test {
     public static void main(String[] args) {
         Path prefix = Paths.get("/home/rany/NBCorpus/Country");
-
+        for(String arg:args){
+            arg = arg.toUpperCase();
+            File file = prefix.resolve(arg).toFile();
+            if(file.exists()){
+                System.out.println(file);
+            }
+        }
     }
 }
--- src/TrainingPreHandle.java
+++ src/TrainingPreHandle.java
@@ -1,23 +1,30 @@
-/**
+/*
 * 输入将要预测的国别名，确定训练预测比例，将本地的文件上传到HDFS，形成训练集和预测集。
 */
 
-import java.net.URI;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
+import java.io.IOException;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.conf.Configuration;
 public class TrainingPreHandle {
     //本地样本的目录
-    static Path localPrefix = Paths.get("/home/rany/NBCorpus/Country");
-    //HDFS的URI
-    static URI hdfsURI = URI.create("hdfs://localhost:9000");
-    //HDFS的训练集所在目录
-    static Path hdfsPrefix = Paths.get(hdfsURI.getPath(),"Training");
+    static Path localPrefix = new Path("/home/rany/NBCorpus/Country");
+    //HDFS上训练集的路径
+    static Path trainingSetPrefixOnHDFS = new Path("/Training");
     //训练集所占比例
     static double trainingRatio = 0.5;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //配置并获取HDFS实例
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+
         //清空HDFS上的Training文件夹
+        if(fs.exists(trainingSetPrefixOnHDFS)){
+            fs.delete(trainingSetPrefixOnHDFS,true);
+        }
+        fs.mkdirs(trainingSetPrefixOnHDFS);
+
         //遍历args参数作为类别
         //获取一个类别文件夹中的文件列表
         //随机选取一个文件移动到HDFS，直到达到训练集所占比例
