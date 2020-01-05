4ac59eebc9911ed24286ddf9bc947cb4331980d2
RanY <978697341@qq.com>
2020-01-05 23:08:16 +0800

    Finish TrainingPreHandle

--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,24 @@
+import com.sun.org.apache.bcel.internal.generic.NEW;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.shaded.com.jcraft.jsch.IO;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.IOException;
 import java.lang.System;
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
 public class Test {
-    public static void main(String[] args) {
-        Path prefix = Paths.get("/home/rany/NBCorpus/Country");
-        for(String arg:args){
-            arg = arg.toUpperCase();
-            File file = prefix.resolve(arg).toFile();
-            if(file.exists()){
-                System.out.println(file);
-            }
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        Path path = new Path("/home/rany/NBCorpus/Country/CHINA");
+        FileSystem localfs = FileSystem.get(new URI("file:///"), new Configuration());
+        for(FileStatus s:localfs.listStatus(path)){
+            System.out.println(s.getPath());
         }
     }
 }
--- src/TrainingPreHandle.java
+++ src/TrainingPreHandle.java
@@ -3,30 +3,80 @@
 */
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.conf.Configuration;
 public class TrainingPreHandle {
     //本地样本的目录
-    static Path localPrefix = new Path("/home/rany/NBCorpus/Country");
+    static Path localSamplePrefix = new Path("/home/rany/NBCorpus/Country");
+    //HDFS的URI
+    static String hdfsURI = "hdfs://localhost:9000";
+    //本地预测集的路径
+    static Path predictionSetPrefixOnLocal = new Path(localSamplePrefix, "Prediction");
     //HDFS上训练集的路径
     static Path trainingSetPrefixOnHDFS = new Path("/Training");
     //训练集所占比例
     static double trainingRatio = 0.5;
 
-    public static void main(String[] args) throws IOException {
-        //配置并获取HDFS实例
-        Configuration conf = new Configuration();
-        FileSystem fs = FileSystem.get(conf);
+    public static void main(String[] args) throws IOException, URISyntaxException {
 
-        //清空HDFS上的Training文件夹
-        if(fs.exists(trainingSetPrefixOnHDFS)){
-            fs.delete(trainingSetPrefixOnHDFS,true);
+        //配置并获取本地文件系统和HDFS实例
+        try (FileSystem localfs = FileSystem.get(new URI("file:///"), new Configuration());
+             FileSystem hdfs = FileSystem.get(new URI(hdfsURI), new Configuration())) {
+            //清空Prediction文件夹
+            if (localfs.exists(predictionSetPrefixOnLocal)) {
+                localfs.delete(predictionSetPrefixOnLocal, true);
+            }
+            localfs.mkdirs(predictionSetPrefixOnLocal);
+
+            //清空HDFS上的Training文件夹
+            if (hdfs.exists(trainingSetPrefixOnHDFS)) {
+                hdfs.delete(trainingSetPrefixOnHDFS, true);
+            }
+            hdfs.mkdirs(trainingSetPrefixOnHDFS);
+
+            //遍历args参数作为类别
+            Path localSampleDir;
+            Path localPredictionDir;
+            Path hdfsTrainingDir;
+
+            //实例化一个随机类对象
+            Random random = new Random();
+            for (String arg : args) {
+                arg = arg.toUpperCase();
+                localSampleDir = new Path(localSamplePrefix, arg);
+                localPredictionDir = new Path(predictionSetPrefixOnLocal, arg);
+                localfs.mkdirs(localPredictionDir);
+                hdfsTrainingDir = new Path(trainingSetPrefixOnHDFS, arg);
+                hdfs.mkdirs(hdfsTrainingDir);
+                if (localfs.exists(localPredictionDir)) {
+                    ArrayList<FileStatus> fileStatuses = new ArrayList<>(Arrays.asList(
+                            localfs.listStatus(
+                                    localSampleDir)));
+                    int countOfTrainingSample = (int) (fileStatuses.size() * trainingRatio);
+                    System.out.println("开始上传训练集" + arg);
+                    while ((countOfTrainingSample--) != 0) {
+                        //随机获取一个文件上传到HDFS
+                        FileStatus chosenFileStatus = fileStatuses.remove(random.nextInt(fileStatuses.size()));
+                        hdfs.copyFromLocalFile(chosenFileStatus.getPath(), hdfsTrainingDir);
+                    }
+                    //将剩下的文件复制到预测文件夹
+                    System.out.println("开始上传预测集" + arg);
+                    for (FileStatus status : fileStatuses) {
+                        localfs.copyFromLocalFile(status.getPath(), localPredictionDir);
+                    }
+                } else {
+                    System.out.println(arg + "not exists");
+                    break;
+                }
+            }
         }
-        fs.mkdirs(trainingSetPrefixOnHDFS);
 
-        //遍历args参数作为类别
-        //获取一个类别文件夹中的文件列表
-        //随机选取一个文件移动到HDFS，直到达到训练集所占比例
     }
 }
